8557472ee51e74014ff2a27f58473ee81c469186
julienYncrea <julien.minvielle@isen-ouest.yncrea.fr>
2024-10-01 16:24:22 +0200

    salut c le commit de juju

--- SVM/supportvectormachine.py
+++ SVM/supportvectormachine.py
@@ -4,9 +4,13 @@ import pandas as pd
 class SupportVectorMachine:
 
     def __init__(self, df):
-        self.__df = df
+        self.df = df
+        self.population = None
+        self.price = None
 
-
+    def extractData(self):
+        self.population = self.df["population"]
+        self.price = self.df["Price"]
 
     def predict(self):
         pass
