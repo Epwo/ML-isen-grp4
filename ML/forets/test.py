400ea4c84226af8fbf7d163ab03307c96019f72b
Nino Pireaud <nino.pireaud@isen-ouest.yncrea.fr>
2024-10-01 17:00:08 +0200

    changements bof

--- ML/forets/test.py
+++ ML/forets/test.py
@@ -1,5 +1,17 @@
-import pandas as pds
+from classeRdForest import Foret
+import numpy as np
 
-df = pds.read_csv("C:\\Users\\ninop\\Documents\\ML-isen-grp4\\data\\Carseats.csv")
-print(df.shape)
+np.random.seed(42)
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X[:, 0] + np.random.randn(100)
+
+foret = Foret()
+foret.fit(X,y)
+
+y_pred = foret.predict(X)
+
+print(y_pred)
+
+
+
 
